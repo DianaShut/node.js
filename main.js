15d6546d71c9d9156151c3a532ceb3810032971f
Diana <diankashy0@gmail.com>
2024-04-23 13:06:04 +0300

    .

--- main.js
+++ main.js
@@ -12,6 +12,40 @@ async function foo() {
 
         const folderNames = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
         const filesNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
+
+        for(const folderName of folderNames) {
+            const folderPath = path.join(basePath, folderName);
+            await fs.mkdir(folderPath, {recursive: true});
+
+            for (const fileName of filesNames) {
+                await fs.writeFile(path.join(folderPath, fileName), 'Hello, World!');
+            }
+        }
+
+        //Перевірка
+        // Читаємо вміст директорії за шляхом, заданим у змінній basePath. result - масив назв файлів та папок, які знаходяться в basePath.
+        const result = await fs.readdir(basePath)
+
+        // Циклічно перебираємо кожен елемент (назву файлу або папки) з отриманого масиву result
+        for (const folderName of result) {
+
+            // Створюємо повний шлях до кожного елемента, об'єднуючи basePath та поточне ім'я з масиву result
+            const folderPath = path.join(basePath, folderName)
+
+            // Отримуємо метадані про файл або папку за допомогою fs.stat, що дозволяє нам дізнатися більше про елемент
+            const stat = await fs.stat(folderPath);
+            // Виводимо в консоль інформацію, чи є поточний елемент директорією
+            console.log('STAT: isDirectory: ', stat.isDirectory());
+
+            //Аналогічно
+            const result = await fs.readdir(folderPath)
+            for (const item of result) {
+                const itemPath = path.join(folderPath, item)
+                const stat = await fs.stat(itemPath);
+                console.log('STAT: isDirectory: ', stat.isDirectory());
+            }
+        }
+        
     }catch (e) {
         console.error(e)
     }
