fb447b80fcd161540e765558b79b11ca9b2edf8d
Diana <diankashy0@gmail.com>
2024-05-01 11:41:41 +0300

    .

--- src/api-error.ts
+++ src/api-error.ts
@@ -1,7 +1,10 @@
+// Оголошення класу ApiError, який наслідує вбудований клас Error.
 export class ApiError extends Error {
+    // Декларація публічної властивості 'status', яка призначена для зберігання HTTP статусу помилки.
     public status: number;
+
     constructor(message: string, status: number) {
-        super(message);
-        this.status = status
+        super(message);  // Виклик конструктора базового класу Error з параметром 'message'.
+        this.status = status;  // Присвоєння значення параметра 'status' властивості 'status'.
     }
 }
